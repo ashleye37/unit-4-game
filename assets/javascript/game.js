7e1fd27fc2a87856be35b5096ea68dc2115624b7
Ashley Evans <ashley_e@live.com>
2019-03-19 17:47:19 -0700

    Fixing bug found in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,9 +27,6 @@ $(document).ready (function() {
         crystal2 = Math.floor(Math.random() * (crystalMax - crystalMin) + crystalMin);
         crystal3 = Math.floor(Math.random() * (crystalMax - crystalMin) + crystalMin);
         crystal4 = Math.floor(Math.random() * (crystalMax - crystalMin) + crystalMin);
-
-        $("#displayWin").text(wins);
-        $("#displayLosses").text(losses);
     };
 
     //Function that will add random crystal value to player's score and display a custom alert of the crystal point value.
@@ -40,8 +37,17 @@ $(document).ready (function() {
             $(".alert-dark").show();
             userTotal = userTotal + crystal1;
             $("#displayScore").text(userTotal);
-            checkForWin();
-            checkForLoss();
+            if (userTotal === randomNumber) {
+                wins++;
+                $("#displayWin").text(wins);
+                winAlerts();
+            };
+            
+            if (userTotal > randomNumber) {
+                losses++;
+                $("#displayLosses").text(losses);
+                lossAlerts();
+            };
         });
 
         $("#green-crystal").on("click", function() {
@@ -50,8 +56,17 @@ $(document).ready (function() {
             $(".alert-dark").show();
             userTotal = userTotal + crystal2;
             $("#displayScore").text(userTotal);
-            checkForWin();
-            checkForLoss();
+            if (userTotal === randomNumber) {
+                wins++;
+                $("#displayWin").text(wins);
+                winAlerts();
+            };
+            
+            if (userTotal > randomNumber) {
+                losses++;
+                $("#displayLosses").text(losses);
+                lossAlerts();
+            };
         });
 
         $("#diamond-crystal").on("click", function() {
@@ -60,8 +75,17 @@ $(document).ready (function() {
             $(".alert-dark").show();
             userTotal = userTotal + crystal3;
             $("#displayScore").text(userTotal);
-            checkForWin();
-            checkForLoss();
+            if (userTotal === randomNumber) {
+                wins++;
+                $("#displayWin").text(wins);
+                winAlerts();
+            };
+            
+            if (userTotal > randomNumber) {
+                losses++;
+                $("#displayLosses").text(losses);
+                lossAlerts();
+            };
         });
 
         $("#blue-crystal").on("click", function() {
@@ -70,38 +94,43 @@ $(document).ready (function() {
             $(".alert-dark").show();
             userTotal = userTotal + crystal4;
             $("#displayScore").text(userTotal);
-            checkForWin();
-            checkForLoss();
+            if (userTotal === randomNumber) {
+                wins++;
+                $("#displayWin").text(wins); 
+                winAlerts();
+            };
+            
+            if (userTotal > randomNumber) {
+                losses++;
+                $("#displayLosses").text(losses);
+                lossAlerts();
+            };
         });
+
+        
     };
 
     //Function that runs to check for a win as the player uses the game.
-    function checkForWin() {
-        if (userTotal === randomNumber) {
-            $(".alert-dark").hide();
-            $(".alert-primary").show();
-            $(".close").on("click", function() {
-                $(".alert-primary").hide();
-                wins++;
-                $("#displayWin").text(wins);
-                reset();
-            });
-        };
+    function winAlerts() {
+        $(".alert-dark").hide();
+        $(".alert-primary").show();
+        $(".close").on("click", function() {
+        $(".alert-primary").hide();
+        reset();
+        });
+    };
+    
+    function lossAlerts() {
+        $(".alert-dark").hide();
+        $(".alert-secondary").show();
+        $(".close").on("click", function() {
+        $(".alert-secondary").hide();
+        reset();
+        });
     };
     
     //Function that runs to check for a loss as the player uses the game.
-    function checkForLoss() {
-        if (userTotal > randomNumber) {
-            $(".alert-dark").hide();
-            $(".alert-secondary").show();
-            $(".close").on("click", function() {
-                $(".alert-secondary").hide();
-                losses++;
-                $("#displayLosses").text(losses);
-                reset();
-            });
-        };
-    };
+    
     //Function that will reset the game so that the player can play again without having to refresh the page and will continue to keep track of their wins and losses.
     function reset() {
         setUpGame();
